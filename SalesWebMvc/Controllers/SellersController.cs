e1acc489e463c88d5064182dd3455f860e4485fc
Administrator@CARLINHOS.COM <Administrator@CARLINHOS.COM>
2020-10-19 21:27:46 -0300

    Edit and Delete

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
+using System.Collections.Generic;
 
 namespace SalesWebMvc.Controllers {
     public class SellersController : Controller {
@@ -44,6 +46,46 @@ namespace SalesWebMvc.Controllers {
         public IActionResult Delete(int id) {
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id) {
+            if (id == null) {
+                return NotFound();
+            }
+            var obj = _sellerService.FindByID(id.Value);
+            if (obj == null) {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id) {
+            if(id == null) {
+                return NotFound();
+            }
+            var obj = _sellerService.FindByID(id.Value);
+            if(obj == null) {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller) {
+            if(id != seller.Id) {
+                return BadRequest();
+            }
+            try {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            } catch (NotFoundException) {
+                return NotFound();
+            } catch (DbConcurrencyException){
+                return BadRequest();
+            }
         }
     }
 }
